b920f44fe330fe5c64fae6aa6d57a2af00a9f495
amandeep-boot <aman509karan@gmail.com>
2025-03-23 21:09:49 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { Canvas } from '@react-three/fiber';
-import React from 'react'
-import './style.css'
+import React from 'react';
+import './style.css';
 import { OrbitControls} from '@react-three/drei';
-
+import { Bloom ,EffectComposer,BrightnessContrast } from '@react-three/postprocessing';
 import Cyl from './Cyl';
 
 function App() {
-  // useTexture()
   return (
-    <Canvas>
-      <OrbitControls/>
-      <ambientLight />
+    <Canvas flat camera={{ fov: 35, position: [0, 0, 5] }}>
+      <OrbitControls />
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 2, 5]} intensity={1} />
       <Cyl />
+      <EffectComposer>
+        <Bloom
+          intensity={5.0} // Strength of bloom
+          luminanceThreshold={0} // Brightness threshold
+          luminanceSmoothing={0} // Smooth the threshold edges
+          mipmapBlur // Makes the bloom look more natural
+        />
+        <BrightnessContrast
+          brightness={0.0}  // 0 = no change, positive = brighter
+          contrast={0.3}   // Increase contrast (try between 0.2 to 0.6)
+        />
+      </EffectComposer>
     </Canvas>
-  )
+  );
 }
 
-export default App; 
+export default App;
--- src/Cyl.jsx
+++ src/Cyl.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
 
 function Cyl() {
-  let tex = useTexture("./cyl.png");
-  return (
-    <mesh>
-      <cylinderGeometry args={[1, 1, 1, 40, 40, true]} />
-      <meshStandardMaterial
-        map={tex}
-        transparent={true}
-        alphaTest={0.5}
-        side={THREE.DoubleSide}
-      />
-      
+  const tex = useTexture("./cyl.png");
+  const cyl = useRef(null);
 
-    </mesh>
+  useFrame((state, delta) => {
+    if (cyl.current) {
+      cyl.current.rotation.y += delta;
+    }
+  });
+
+  return (
+    <group rotation={[0, 1.4, 0.25]}>
+      <mesh ref={cyl}>
+        <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
+        <meshStandardMaterial
+          map={tex}
+          transparent={true}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+    </group>
   );
 }
 
